b0c0154aab7fbf7bfd4faf63b09038c2561f9817
prakharDvedi <prakhardwivedi.12@gmail.com>
2026-01-09 11:15:18 +0530

    Docker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ import { currentWorkData } from "@/data/currentWork";
 export default function Home() {
   const [time, setTime] = useState<string>("");
   const words = useMemo(
-    () => ["Next.js • FastAPI • Prisma • React • Tailwind"],
+    () => ["Next.js • FastAPI • Prisma • React • Docker"],
     []
   );
 
